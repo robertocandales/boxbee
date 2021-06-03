61630070f01f084e7a6e8b41b3064c8a752c5640
Roberto Candales <candales.roberto@gmail.com>
2021-06-03 14:50:16 -0300

    adding data

--- src/screens/shared/customizedInput/CardsInput.js
+++ src/screens/shared/customizedInput/CardsInput.js
@@ -3,31 +3,31 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
     },
+  },
 }));
 
-const CardsInput = () => {
-    const classes = useStyles();
-    return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <div>
-                <TextField
-                    id="outlined-number"
-                    label="Box name"
-                    type="text"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="outlined"
-                />
-            </div>
-        </form>
-    );
-}
+const CardsInput = ({ description = 'add description' }) => {
+  const classes = useStyles();
+  return (
+    <form className={classes.root} noValidate autoComplete='off'>
+      <div>
+        <TextField
+          id='outlined-number'
+          label={description}
+          type='text'
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant='outlined'
+        />
+      </div>
+    </form>
+  );
+};
 
-export default CardsInput
+export default CardsInput;
