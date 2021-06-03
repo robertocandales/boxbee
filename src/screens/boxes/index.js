8a9e7c892135a3385c1488bd8073d10ae23cd97a
edgarcandales <68811563+edgarcandales@users.noreply.github.com>
2021-06-03 10:30:24 -0400

    start charts

--- src/screens/boxes/index.js
+++ src/screens/boxes/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { WrapperCard, InnerCard } from './styles';
 
 const Boxes = () => {
-  return (
-    <div>
-      {' '}
+    return (
+        <div>
+
+            {' '}
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis in cupiditate
       perspiciatis, accusamus esse doloremque, commodi, iure excepturi similique necessitatibus rem
       eos tempore. Itaque obcaecati corrupti non suscipit tempora nostrum?{' '}
-    </div>
-  );
+            <WrapperCard>Edgar
+                <InnerCard> sdf</InnerCard>
+            </WrapperCard>
+        </div>
+    );
 };
 
 export default Boxes;
---
+++ src/screens/boxes/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const WrapperCard = styled.div`
+  width: 80%;
+  height: 80%vh;
+  padding: 32px 30px 129px 52px;
+  border-radius: 50px;
+  background-color: #ffffff; 
+`;
+
+export const InnerCard = styled.div`
+  width: 30%;
+  height: 50%;
+  margin: 65px 36px 32px 0;
+  padding: 15px 25px 165px 28px;
+  border-radius: 25px;
+  box-shadow: 0 10px 40px -10px rgba(0, 0, 0, 0.3);
+  background-color: #ffffff; 
+`;
+
