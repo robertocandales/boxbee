239a6d4eac4fb806f9a95bf5138bcf2e0a516874
edgarcandales <68811563+edgarcandales@users.noreply.github.com>
2021-06-03 12:19:59 -0400

    PR master

--- src/screens/shared/button/styles.js
+++ src/screens/shared/button/styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import { colors } from '../../../config/theme';
+
 
 export const CustomizedButton = styled(Button)`
-  background-color: goldgenrod;
+  background-color: ${colors.GOLD};
 
 `;
