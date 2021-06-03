fd029b72f3fd40d4b5d9241463703e0294d03e48
Roberto Candales <candales.roberto@gmail.com>
2021-06-03 20:06:39 -0300

    step bar fixes git conflit

--- src/screens/boxes/index.js
+++ src/screens/boxes/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { WrapperCard, Title, WrapperButton, CardsFlex } from './styles';
 import CardButton from '../shared/button/CardButton';
+import StepperComponent from './components/stepper';
 
 //data
 import data from './data/index.json';
 import SmallerCards from './components/SmallerCard/SmallerCards';
 
 const Boxes = () => {
-  console.log(data);
   return (
     <>
       <WrapperCard>
+        <StepperComponent />
         <Title>Step 1: Set up your box</Title>
         <CardsFlex>
           {(data.data || []).map((item, index) => (
